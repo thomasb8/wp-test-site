1ffc9bc7a65f68e0548ad1d882f2360efddebdea
Tamas Batka <batkatom@gmail.com>
2024-06-21 02:19:51 +0200

    add dynamic menu to header and footer

--- themes/cecils-theme/resources/views/sections/footer.blade.php
+++ themes/cecils-theme/resources/views/sections/footer.blade.php
@@ -1,29 +1,25 @@
 <footer class="bg-gray-400 text-white font-pill">
   <div class="container mx-auto">
     <div class="flex justify-between items-start pt-14 pb-24 md:pb-8">
-    <img src="@asset('/images/Cecils_LogoIcon.svg')" class="w-[70px] md:w-[105px] " alt="Logo" />
-    <nav class="text-right">
-      <ul class="flex flex-col md:flex-row gap-1 md:gap-8 font-black text-[1.125rem]">
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
+      <img src="@asset('/images/Cecils_LogoIcon.svg')" class="w-[70px] md:w-[105px] " alt="Logo" />
+
+      @if (has_nav_menu('primary_navigation'))
+        <nav class="text-right" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        {!! wp_nav_menu([
+          'theme_location' => 'primary_navigation',
+          'menu_class' => 'flex flex-col md:flex-row gap-1 md:gap-8 font-black text-[1.125rem]',
+          'container' => '',
+          'echo' => false
+        ]) !!}
+        </nav>
+    @endif
+
     </div>
     <div class="text-base font-sans w-full text-center md:text-right relative md:-top-14 pb-16 md:pb-0">
-        ©{{date('Y')}} Cecil’s Concrete
-        <span class="hidden md:inline"> / </span>
-        <br class="md:hidden" />
-        <a href="#">Privacy Policy</a>
-      </div>
+      ©{{date('Y')}} Cecil’s Concrete
+      <span class="hidden md:inline"> / </span>
+      <br class="md:hidden" />
+      <a href="#">Privacy Policy</a>
+    </div>
   </div>
 </footer>
--- themes/cecils-theme/resources/views/sections/header.blade.php
+++ themes/cecils-theme/resources/views/sections/header.blade.php
@@ -1,35 +1,24 @@
 <header class="banner absolute left-0 top-0 w-full z-10">
-  <div class="container mx-auto h-[7.25rem] flex justify-between items-center font-pill text-white font-semibold leading-none">
+  <div
+    class="container mx-auto h-[7.25rem] flex justify-between items-center font-pill text-white font-semibold leading-none">
     <a class="brand" href="{{ home_url('/') }}">
-      <img class="cecils-logo" src="@asset('images/Cecils Concrete_Logo._White.svg')"/ alt="Cecils Logo">
+      <img class="cecils-logo" src="@asset('images/Cecils Concrete_Logo._White.svg')" / alt="Cecils Logo">
     </a>
     <button class="nav-toggle">
       <span class="nav-toggle-icon"></span>
     </button>
 
-    <nav class="nav-primary hidden md:flex w-full justify-end">
-      <ul class="flex flex-col md:flex-row gap-10 md:gap-4 lg:gap-14 justify-center md:justify-between items-center text-[22px] md:text-[1.25rem]">
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-        <button class="bg-light-blue px-4 py-3 rounded-full text-[18px] md:text-[1.25rem]">(205) 480-2083</button>
-      </ul>
-    </nav>
+    @if (has_nav_menu('primary_navigation'))
+      <nav class="nav-primary hidden md:flex w-full justify-end"
+        aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        {!! wp_nav_menu([
+      'theme_location' => 'primary_navigation',
+      'menu_class' => 'flex flex-col md:flex-row gap-10 md:gap-4 lg:gap-14 justify-center md:justify-between items-center text-[22px] md:text-[1.25rem]',
+      'container' => '',
+      'items_wrap' => '<ul id="%1$s" class="%2$s>%3$s<a class="bg-light-blue px-4 py-3 rounded-full text-[18px] md:text-[1.25rem]" href="tel:(205) 480-2083">(205) 480-2083</a></ul>',
+      'echo' => false
+      ]) !!}
+      </nav>
+    @endif
   </div>
-
-
-  <!-- @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif -->
 </header>
